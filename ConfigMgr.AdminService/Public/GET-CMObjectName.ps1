e2d2825dab2902c2e22c5b0703c91c2bb60bea4a
Sam Beckler <BECKLE2@clemson.edu>
2022-12-07 11:47:28 -0500

    Create GET-CMObjectName.ps1
    Gets sms_objectname class

---
+++ ConfigMgr.AdminService/Public/GET-CMObjectName.ps1
@@ -0,0 +1,19 @@
+function Get-CMASObjectName {
+    [cmdletbinding()]
+    param (
+        [string]$ObjectKey
+    )
+
+    try {
+        $FilterObjs = foreach ($key in ($PSBoundParameters.keys | Where-Object { $_ -notin ("Verbose","Debug") })) {
+            Get-FilterObject $Key $PSBoundParameters[$key]
+        }
+        $Filter = $FilterObjs | Get-FilterString
+        Invoke-CMGet -URI "$($script:ASWmiURI)SMS_ObjectName$($Filter)" -verbose
+
+        return $Result | Select-Object -Property * -ExcludeProperty _*, `@odata*
+    }
+    catch {
+
+    }
+}
