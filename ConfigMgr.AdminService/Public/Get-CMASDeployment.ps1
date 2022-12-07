22bcfda73d8de8ae6b140bf3d85fd27d567f93f2
Sam Beckler <BECKLE2@clemson.edu>
2022-12-07 16:34:28 -0500

    Update Get-CMASDeployment.ps1
    change to get sms_deploymentinfo class

--- ConfigMgr.AdminService/Public/Get-CMASDeployment.ps1
+++ ConfigMgr.AdminService/Public/Get-CMASDeployment.ps1
@@ -1,19 +1,24 @@
 function Get-CMASDeployment {
     [cmdletbinding()]
     param (
-        [string]$CollectionID
+        [string]$CollectionID,
+        [string]$DeploymentID
     )
 
     try {
-        $FilterObjs = foreach ($key in ($PSBoundParameters.keys | Where-Object { $_ -notin ("Verbose","Debug") })) {
-            Get-FilterObject $Key $PSBoundParameters[$key]
+        $Result = if ($DeploymentID) {
+            Invoke-CMGet -URI "$($script:ASWmiURI)SMS_DeploymentInfo($DeploymentID)"    
         }
-        $Filter = $FilterObjs | Get-FilterString
-        Invoke-CMGet -URI "$($script:ASWmiURI)SMS_Advertisement$($Filter)" -verbose
-
+        else {
+          $FilterObjs = foreach ($key in ($PSBoundParameters.keys | Where-Object { $_ -notin ("Verbose","Debug") })) {
+              Get-FilterObject $Key $PSBoundParameters[$key]
+          }
+          $Filter = $FilterObjs | Get-FilterString
+          Invoke-CMGet -URI "$($script:ASWmiURI)SMS_DeploymentInfo$($Filter)"
+        }
         return $Result | Select-Object -Property * -ExcludeProperty _*, `@odata*
     }
     catch {
-
+      throw $_
     }
 }
