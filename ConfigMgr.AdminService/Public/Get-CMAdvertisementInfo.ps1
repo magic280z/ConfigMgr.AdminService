bd76a4d8f071075099c123bdd67d7435e2938501
Sam Beckler <74254250+magic280z@users.noreply.github.com>
2023-03-22 15:19:08 -0400

    Added support for advertisementid array for faster retreival of multiple advertisements.

--- ConfigMgr.AdminService/Public/Get-CMAdvertisementInfo.ps1
+++ ConfigMgr.AdminService/Public/Get-CMAdvertisementInfo.ps1
@@ -2,18 +2,32 @@ function Get-CMAdvertisementInfo {
     [cmdletbinding()]
     param (
         [string]$CollectionID,
-        [string]$AdvertisementID
+        [string[]]$AdvertisementID
     )
 
     try {
-        $Result = if ($AdvertisementID) {
+        $Result = if ($AdvertisementID.Count -eq 1) {
+          Write-Verbose "direct get"
             Invoke-CMGet -URI "$($script:ASWmiURI)SMS_AdvertisementInfo('$AdvertisementID')"
         }
-        else {
+        elseif ($AdvertisementID -is [array]) {
+
+          write-verbose "advertisement array"
+
+          $FilterObjs = $AdvertisementID | foreach-object {get-filterobject "AdvertisementID" $_}
+
+          #$FilterObjs
+
+          $Filter = Get-FilterString -jointype or -FilterObjs $FilterObjs
+
+          #$filter
+          Invoke-CMGet -URI "$($script:ASWmiURI)SMS_AdvertisementInfo$($Filter)"
+        } else {
+          Write-Verbose "building filter"
           $FilterObjs = foreach ($key in ($PSBoundParameters.keys | Where-Object { $_ -notin ("Verbose","Debug") })) {
               Get-FilterObject $Key $PSBoundParameters[$key]
           }
-          $Filter = $FilterObjs | Get-FilterString
+          $Filter = $FilterObjs | Get-FilterString 
           Invoke-CMGet -URI "$($script:ASWmiURI)SMS_AdvertisementInfo$($Filter)"
         }
         return $Result | Select-Object -Property * -ExcludeProperty _*, `@odata*
